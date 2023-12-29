dca0ce1b35271d4a44e118f17efcefccfe2c9326
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-12-29 13:25:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import logo from "./shecodes-logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="app-header">
@@ -15,5 +15,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
