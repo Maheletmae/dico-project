359e383cd151af6197d727bd026f97e6ba736329
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-12-29 14:02:48 +0100

    Adding the template (initial code with containers) for a dictionary javascript

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import logo from "./shecodes-logo.png";
 export default function App() {
   return (
     <div className="App">
-      <header className="app-header">
-        <img
-          src={logo}
-          className="app-logo img-fluid"
-          alt="shecodes logo"
-        ></img>
-      </header>
-      <a className="btn btn-primary">Hello</a>
+      <div className="container">
+        <header className="app-header">
+          <img
+            src={logo}
+            className="app-logo img-fluid"
+            alt="shecodes logo"
+          ></img>
+        </header>
+        <a className="btn btn-primary">Hello</a>
+        <footer>Coded by Mahelet for SheCodes boothcamp</footer>
+      </div>
     </div>
   );
 }
---
+++ src/DicoSearch.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DicoSearch() {
+  <div className="DicoSearch">Hello from Dico javascript code</div>;
+}
