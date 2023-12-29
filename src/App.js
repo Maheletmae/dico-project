53baa3f6eda7493ecad91502da8fa0faea961845
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-12-29 12:30:04 +0100

    Updated the logo in App.js
    Removed previous unnecessary logos and put a new one in the src folder. Then added it in the header of App.js (import and img code)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import "./SheCodes-logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="app-header">
+        <img src="{logo}" className="app-logo" alt="shecodes logo"></img>
+      </header>
+    </div>
+  );
 }
 
 export default App;
