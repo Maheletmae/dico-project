d6200c1acba471ba6029448cad7c9fb1f9e719f3
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-12-29 16:21:21 +0100

    Rendered the word meanings

--- src/DicoSearch.js
+++ src/DicoSearch.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./DicoSearch.css";
+import Results from "./Results";
 
 export default function DicoSearch() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -15,14 +17,17 @@ export default function DicoSearch() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="DicoSearch">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange}></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
